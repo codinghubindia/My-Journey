4391df2ab43bb0a16a1eac046ef9e43e37eaa350
codinghubindia <manjun2412@gmail.com>
2024-09-27 15:37:50 +0530

    Stack Program

--- cpp/stack/stack.c
+++ cpp/stack/stack.c
@@ -1,10 +1,14 @@
 #include<stdio.h>
+#include<string.h>
 
 int stack[100];
 int top = -1;
 
-void push(int elem) {
-    stack[top++] = elem;
+void push() {
+    int elem;
+    printf("Enter the Element to Push\n");
+    scanf("%d",&elem);
+    stack[++top] = elem;
     return;
 }
 
@@ -12,14 +16,58 @@ void pop() {
     if(top == -1) {
         printf("Stack is Empty");
     } else {
-        top--;
+        --top;
     }
 }
 
+void pallindrome() {
+    char ar[100];
+    printf("Enter the Numbers: ");
+    scanf("%s", ar);
+    int i = 0;
+    int len = strlen(ar);
+    int j = len-1;
+    for(i; i < (len-1) / 2 ; i++){
+        if(ar[i] != ar[j] ){
+            printf("It is not a Pallindrom\n");
+            return;
+        }
+        j--;
+    }
+    printf("It is a Pallindrome\n");
+    return;
+}
+
 void display () {
-    for(int i = top; i >= 0; i--) printf("%d",stack[i]);
+    if(top != -1)
+        for(int i = top; i >= 0; i--) printf("%d\t",stack[i]);
+    else printf("Stack is Empty\n");
 }
 
 int main() {
-    
+    while(1) {
+        int a;
+        printf("Enter from Following\n1 - Push\n2 - Pop\n3 - Display\n4 - Check Pallindrom\n5 - Exit\n");
+        scanf("%d", &a);
+
+        if(a == 5) break;
+
+        switch(a) {
+            case 1:
+                push();
+                break;
+            case 2:
+                pop();
+                break;
+            case 3:
+                display();
+                break;
+            case 4:
+                pallindrome();
+                break;
+            default:
+                printf("Invalid Option\n");
+        }
+    }
+    return 0;
 }
