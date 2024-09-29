caf0c15cc8e441222976cea0d22ee2ce40fe1904
codinghubindia <maxx.codinghubindia@gmail.com>
2024-09-29 23:11:56 +0530

    Postfix eval:

--- cpp/stack/postfixeval.c
+++ cpp/stack/postfixeval.c
@@ -1,5 +1,41 @@
 #include<stdio.h>
+#include<stdlib.h>
 
-#define MAX 100
+#define max 100
 
-char stack[MAX];
+char stack[max];
+
+int top = -1;
+
+void push(int elem) {
+    if(top >= max - 1) {
+        printf("Stack Overflow\n");
+        return;
+    }
+    stack[++top] = elem;
+}
+
+int pop() {
+    if(top < 0) {
+        printf("Stack is Empty\n");
+        return -1;
+    }
+    int item = stack[top--];
+    return item;
+}
+
+int operator(char ch) {
+    if(ch == '+' || ch == '-' || ch == '*' || ch == '/') {
+        return 1;
+    }
+    return 0;
+}
+
+int evaluate(char *exp) {
+    int i = 0;
+    char ch = exp[i];
+    int num1, num2, result;
+    while(ch != '\0') {
+        
+    }
+}
